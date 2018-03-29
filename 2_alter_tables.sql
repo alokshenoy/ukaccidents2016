aeb028a4f36711104e1420b9a1ff087be21714d7
Alok Shenoy <contact@alokshenoy.me>
2018-03-29 07:45:26 -0600

    Fixed the date of accident in the incidents_2016 table such that it is represented in the ISO-8610,SQL Standard format

--- 2_alter_tables.sql
+++ 2_alter_tables.sql
@@ -1,4 +1,10 @@
 UPDATE incidents_2016
 SET "Time" = CAST("Time" AS TIME)
 
+UPDATE incidents_2016
+SET "Date" = to_date("Date", 'DD/MM/YYYY')
+
+UPDATE incidents_2016
+SET "Date" = replace("Date",'0016','2016')
+
 
